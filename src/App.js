5eac46629a30b1deb6fa1079f33bdfad5697faf2
Harukaichii <h445chen@edu.uwaterloo.ca>
2018-05-06 10:14:42 -0400

    updating things

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ import MapContainer from './mapContainer.js'
 
 const divStyle = {
   height: '250px',
-  width: '100%'
+  width: '100%',
+  position: 'relative'
 }
 
 const borderStyle={
@@ -16,18 +17,82 @@ const borderStyle={
 
 const steps = [
   {
-    id: '1',
-    message: 'Hello World',
-    trigger: '2',
+    id: '0',
+    message: "Greetings :)! I am Hope, a chatbot for Women's Shelter Services. How can I assist you?",
+    trigger: '1',
   },
   {
-    id: '2',
+    id: '1',
+    options: [
+      { value: '1', label: 'Crisis Counselling?', trigger: 'info' },
+      { value: '2', label: 'Legal Services?', trigger: 'legalinfo' },
+      { value: '4', label:  'Help Me Find a Place!', trigger: 'location' },
+      { value: '6', label:  "I'm ok now!", trigger: 'finish' }
+      // { value: '6', label:  'Medical Services?', trigger: 'info' }
+    ]
+  },
+  {
+    id: 'location',
+    message: 'Alright, can you tell us the nearest city?',
+    trigger:'user_location'
+  },
+  {
+    id: 'user_location',
+    user: true,
+    trigger:'map1'
+
+  },
+  {
+    id: 'map1',
+    message: 'Here are all of the nearby shelters within 10000m of {previousValue}',
+    trigger:'map'
+  },
+  {
+    id: 'map',
     component:(
       <div style={divStyle} >
         <MapContainer/>
       </div>
     ),
     end: true,
+  },
+  {
+    id:'info',
+    message:'Please call 311 to get assistance and/or check this website for more info ',
+    trigger: 'help-link'
+
+  },
+  {
+    id:'help-link',
+    component:(<a href="https://www.toronto.ca/311/knowledgebase/kb/docs/articles/311-toronto/information-and-business-development/crisis-lines-suicide-depression-telephone-support-lines-non-crisis-mental-health-services.html">
+  Click Me!</a>),
+  trigger:'prompt'
+
+  },
+  {
+    id:'legalinfo',
+    message:'In need of an immediate lawyer please contact our partner at LegalAid',
+    trigger:'legallink'
+  },
+  {
+    id:'legallink',
+    component:(<a href="https://www.legalaid.on.ca/en/getting/type_domesticviolence.asp">
+  Click Me!</a>),
+    trigger:'legalinfo2'
+  },{
+    id:'legalinfo2',
+    message:'Additionally, the Assaulted Women’s Helpline is available 24 hours a day, 7 days a week at 1-866-863-0511',
+    trigger:'prompt'
+  },
+  {
+    id:'prompt',
+    message:'is there anything else we can help you with?',
+    trigger: '1',
+  },
+  {
+    id: 'finish',
+    message: 'Take care and have a great day!',
+    end: true
   }
 ];
 
@@ -36,7 +101,7 @@ class App extends Component {
     return (
          <center>
           <ChatBot
-            headerTitle="App"
+            headerTitle="HopeBot"
             botAvatar= {Image}
              steps={steps}
              style= {borderStyle}
--- src/mapContainer.js
+++ src/mapContainer.js
@@ -32,10 +32,55 @@ export class MapContainer extends Component {
     }
     return (
       <Map google={this.props.google}
-      style={style} zoom={14}>
+      style={style} zoom={14}
+      initialCenter={{
+            lat: 43.6532,
+            lng: -79.3832
+          }}
+          onClick={this.onMapClicked}>
 
         <Marker onClick={this.onMarkerClick}
-                name={'Current location'} />
+                name={"Women's Habitat"}
+                position={{lat: 43.6011776,
+                lng: -79.5078087}}/>
+
+      <Marker onClick={this.onMarkerClick}
+              name={"Nellie's Administration"}
+              position={{lat: 43.6593677,
+              lng: -79.3502875}} />
+
+              <Marker onClick={this.onMarkerClick}
+                      name={"Red Door Family Shelter"}
+                      position={{lat: 43.656403,
+                      lng: -79.3401427}} />
+
+                      <Marker onClick={this.onMarkerClick}
+                              name={"Sistering\n Serving:45\n Capacity: 50"}
+                              position={{lat: 43.6614163,
+                              lng: -79.4309733}} />
+                              <Marker onClick={this.onMarkerClick}
+                                      name={"Street Haven at The Crossroads"}
+                                      position={{lat: 43.6592425,
+                                      lng: -79.3746183}} />
+                                      <Marker onClick={this.onMarkerClick}
+                                              name={"Covenant House Toronto"}
+                                              position={{lat: 43.6593677,
+                                              lng: -79.3502875}} />
+                                              <Marker onClick={this.onMarkerClick}
+                                                      name={"North York Women's Shelter"}
+                                                      position={{lat: 43.6593677,
+                                                      lng: -79.3502875}} />
+                                                      <Marker onClick={this.onMarkerClick}
+                                                              name={"Bethlehem United Shelter"}
+                                                              position={{lat: 43.6593677,
+                                                              lng: -79.3502875}} />
+                                                              <Marker onClick={this.onMarkerClick}
+                                                                      name={"Eva's Satellite"}
+                                                                      position={{lat: 43.6593677,
+                                                                      lng: -79.3502875}} />
+
+
+
 
         <InfoWindow
           marker={this.state.activeMarker}
