2d0ada3ed0e3c78994b7d92b6989d53e9ab6d020
MandyMeindersma <meinders@ualberta.ca>
2018-05-05 20:41:04 -0600

    theme good

--- src/App.js
+++ src/App.js
@@ -4,18 +4,6 @@ import ChatBot from 'react-simple-chatbot';
 import Image from './dot.png'
 import MapContainer from './mapContainer.js'
 
-const theme = {
-  background: '#f5f8fb',
-  fontFamily: 'Helvetica Neue',
-  headerBgColor: '#EF6C00',
-  headerFontColor: '#fff',
-  headerFontSize: '15px',
-  botBubbleColor: '#EF6C00',
-  botFontColor: '#fff',
-  userBubbleColor: '#fff',
-  userFontColor: '#4a4a4a',
-};
-
 const style = {
     height: '100px'
 }
@@ -36,20 +24,15 @@ const steps = [
 ];
 
 class App extends Component {
-
   render() {
     return (
-         < ThemeProvider theme = {theme}>
          <center>
           <ChatBot
             headerTitle="App"
             botAvatar= {Image}
              steps={steps}
            />
-
            </center>
-
-        </ThemeProvider>
    );
  }
 
